6f7558b2c53b0167d17b733846002291e0e35479
Yaroslav Furman <yaro330@gmail.com>
2020-03-06 03:15:16 +0300

    drivers: fix a few string related warnings
    ../drivers/thermal/thermal_core.c:1693:8: warning: 'snprintf' size argument is too large; destination buffer has size 128, but size argument is 4096 [-Wfortify-source]
            ret = snprintf(boost_buf, PAGE_SIZE, buf);
                  ^
    1 warning generated.
    ../drivers/soc/qcom/msm_minidump.c:297:39: warning: size argument in 'strlcpy' call appears to be size of the source; expected the size of the destination [-Wstrlcpy-strlcat-size]
            strlcpy(banner, linux_banner, strlen(linux_banner) + 1);
                                          ~~~~~~~^~~~~~~~~~~~~~~~~
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1684,7 +1684,7 @@ static ssize_t
 thermal_boost_show(struct device *dev,
 struct device_attribute *attr, char *buf)
 {
-	return snprintf(buf, PAGE_SIZE, boost_buf);
+	return snprintf(buf, 128, boost_buf);
 }
 
 static ssize_t
@@ -1693,7 +1693,7 @@ struct device_attribute *attr, const char *buf, size_t len)
 {
 	int ret;
 
-	ret = snprintf(boost_buf, PAGE_SIZE, buf);
+	ret = snprintf(boost_buf, 128, buf);
 	return len;
 }
 
